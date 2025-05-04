a19a3d7b36a9fef89cc5d30c4f5c26f8dd23b68a
Dagrion <196099452+Dagrion@users.noreply.github.com>
2025-05-04 20:31:21 +0200

    somme changes

--- src/main/java/dagrion/the_world_rift/mixin/ItemRendererMixin.java
+++ src/main/java/dagrion/the_world_rift/mixin/ItemRendererMixin.java
@@ -9,7 +9,11 @@ import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.ModelIdentifier;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
@@ -73,14 +77,6 @@ public abstract class ItemRendererMixin {
     }
 
 
-
-    @ModifyVariable(method = "renderItem", at = @At(value = "HEAD"), argsOnly = true)
-    public BakedModel useBloodyTridentModel(BakedModel value, ItemStack stack, ModelTransformationMode renderMode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        if (stack.isOf(ModItems.BLOODY_TRIDENT) && renderMode != ModelTransformationMode.GUI) {
-            return ((ItemRendererAccessor) this).mccourse$getModels().getModelManager().getModel(new ModelIdentifier(TheWorldRift.MOD_ID, "bloody_trident_3d", "inventory"));
-        }
-        return value;
-    }
 
 }
 
--- src/main/java/dagrion/the_world_rift/mixin/ModelLoaderMixin.java
+++ src/main/java/dagrion/the_world_rift/mixin/ModelLoaderMixin.java
@@ -58,9 +58,4 @@ public abstract class ModelLoaderMixin {
 
 
 
-    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/ModelLoader;addModel(Lnet/minecraft/client/util/ModelIdentifier;)V", ordinal = 3, shift = At.Shift.AFTER))
-    public void addBloodyTrident(BlockColors blockColors, Profiler profiler, Map<Identifier, JsonUnbakedModel> jsonUnbakedModels, Map<Identifier, List<ModelLoader.SourceTrackedData>> blockStates, CallbackInfo ci) {
-        this.addModel(new ModelIdentifier(TheWorldRift.MOD_ID, "bloody_trident_3d", "inventory"));
-    }
-
 }
